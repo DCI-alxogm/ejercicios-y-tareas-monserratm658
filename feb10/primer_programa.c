d0770d814abf82f437d387ae0310216f093167c3
monserratm658 <im.martinezmorales@ugto.mx>
2025-02-12 12:01:21 -0600

    Variables

---
+++ feb10/primer_programa.c
@@ -0,0 +1,12 @@
+/*creado el 10 de febrero 2025 por monserrat martinez*/
+
+#include<stdio.h> 
+
+int main(){
+
+  printf("Hola Monse \n");
+  printf("Este es el primer programa del curso\n");
+  
+  }
+  
+  /*Termine de escribir el programa*/
