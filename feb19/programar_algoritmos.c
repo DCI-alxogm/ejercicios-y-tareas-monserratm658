57a106d7e8d61333df991e5d13ea100b2bda693a
monserratm658 <im.martinezmorales@ugto.mx>
2025-02-19 11:19:46 -0600

    programar con algoritmos

---
+++ feb19/programar_algoritmos.c
@@ -0,0 +1,15 @@
+/* programa 19 de febrero del 2025: programar con algoritmos pseudocodigo y diagrama de flujo */
+
+
+#include <stdio.h> //Librerias entrada y salida
+#include <stdio.h>  //uso estandar de c
+
+int main(){
+    float TC, TK;  //DECLARO TC Y TK
+    //cuerpo del programa 
+    printf("convierte la TC a TK \n");
+    scanf("%f", &TC);  //lee TC,
+    TK= TC+273.15;     //realiza la operacion
+    printf("la T EN TK ES: %F  \n", TK);
+    
+    }
