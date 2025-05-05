15cfcfa95fcfc4bd21e6f8184086524cb687ea83
monserratm658 <im.martinezmorales@ugto.mx>
2025-05-05 11:59:34 -0600

    parabola

---
+++ 5mayo/programa.c
@@ -0,0 +1,21 @@
+include<stdio.h>
+ 
+ int main() {
+ 	
+ 	float lim_inf=-3, lim_sup=6, delta;
+ 	int n=3
+ 	char op [2]
+ 	
+ 	op[0]= 's';
+ 	delta=((lim_inf)-(lim_sup))/2/n;
+ 	while(op[0]=='s'){
+ 		lim_inf=0
+ 		lim_sup=inicial;
+ 		}
+ 	while(lim_sup<=lim_inf){
+ 		x^,2+1;
+ 		printf("%f %f\n", lim_inf, lim_sup);
+ 		lim_inf=lim_sup+delta;
+ 		}
+ 		return 0;
+ }
